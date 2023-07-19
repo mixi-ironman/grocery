ff4439595666d5abba4ab37e847a39e3fa294c8e
ik53phamvannam@gmail.com <ik53phamvannam@gmail.com>
2023-07-19 08:52:59 +0700

    add layout client

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -14,10 +14,17 @@ class CategoryFactory extends Factory
      *
      * @return array<string, mixed>
      */
+    protected $model = \App\Models\Category::class;
     public function definition(): array
     {
-        return [
-            //
+        return [ 
+            'name' => $this->faker->title,
+            'slug' => $this->faker->slug,
+            'description' => $this->faker->paragraph,
+            'parent_id' => $this->faker->numberBetween($min = 0, $max = 1),
+            'is_active' => $this->faker->boolean(80), // Sử dụng faker để tạo giá trị true/false với tỷ lệ 80% true
+            'updated_at' => now(),
+            'created_at' => now(),
         ];
     }
 }
