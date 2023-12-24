5ec35c9c6a2426909e06a1fb9a77f39d46ddd4a7
mockingbitch <jarvis.phongtran@gmail.com>
2023-12-24 22:33:41 +0700

    feature/dompdf: fix generate pdf

--- app/Http/Controllers/OrderExportExcellController.php
+++ app/Http/Controllers/OrderExportExcellController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Maatwebsite\Excel\Facades\Excel;
 use App\Exports\OrderExcel;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class OrderExportExcellController extends Controller
 {
@@ -13,5 +15,24 @@ class OrderExportExcellController extends Controller
         return Excel::download(new OrderExcel, 'statistics.xlsx');
         // return Excel::download(new OrderExcel, 'filename.xlsx')->headers->set('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
 
+    }
+
+    /**
+     * Generate PDF
+     *
+     * @return Response
+     */
+    public function toPDF(): \Illuminate\Http\Response
+    {
+        $data = ['title' => 'Welcome to Viet Nam'];
+        $pdf = Pdf::loadView('pdf/test', $data);
+        //Nếu muốn hiển thị file pdf theo chiều ngang
+        // $pdf->setPaper('A4', 'landscape');
+
+        //Nếu muốn download file pdf
+        return $pdf->download('myPDF.pdf');
+
+        //Nếu muốn preview in pdf
+        //return $pdf->stream('myPDF.pdf');
     }
 }
